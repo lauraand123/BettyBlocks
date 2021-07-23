b69077ed2d06b8e134b8bd7df03496add87c1284
lauraamd123 <laura_andreescu10@yahoo.com>
2021-07-23 12:43:18 +0300

    created page object model

--- PageModels/CreateAccount.js
+++ PageModels/CreateAccount.js
@@ -7,28 +7,53 @@ class CreateAccount {
         this.lastNameInput = Selector('input').withAttribute('name', 'last_name');
         this.emailInput = Selector('input').withAttribute('name', 'email_address');
         this.passwordInput = Selector('input').withAttribute('name', 'password');
-        this.createAccountButton = Selector('button');
+        this.createAccountButton = Selector('button').withAttribute('type', 'submit');
     }
 
-    async submitUserAccount(options = {}) {
-        this.lastName=options.lastName
+    async createNewUserAccount(options = {}, submitForm = true) {
+        this.lastName = options.lastName;
+        this.firstName = options.firstName;
+        this.email = options.email;
+        this.password = options.password;
+
         // Maximize window, check create account link is available and go to create a new account
         await t.maximizeWindow()
-               .expect(this.registerAccountLink.visible).ok()
-               .click(this.registerAccountLink)
-               // Check user is redirected to the expected page
-               .expect(Selector('h3').innerText).eql('Create new account');
+            .expect(this.registerAccountLink.visible).ok()
+            .click(this.registerAccountLink)
+            // Check user is redirected to the expected page
+            .expect(Selector('h3').innerText).eql('Create new account');
 
         // Fill in registration fields
-        await t.typeText(this.firstNameInput, 'fsfsfs')
-                .typeText(this.lastNameInput, this.lastName)
-                .typeText(this.emailInput, 'Peter')
-                .typeText(this.passwordInput, 'Peter')
-                // Confirm registration
-                .click(this.createAccountButton)
+        if (options.firstName) await t.typeText(this.firstNameInput, this.firstName);
+        if (options.lastName) await t.typeText(this.lastNameInput, this.lastName);
+        if (options.email) await t.typeText(this.emailInput, this.email);
+        if (options.password) await t.typeText(this.passwordInput, this.password);
 
-        const firstNameError = await Selector('label').withText('Email address').parent(0).find('p');
-        await t.expect(firstNameError.innerText).eql('No valid value provided')
+        // Confirm send form if true
+        if (submitForm) await t.click(this.createAccountButton);
+    }
+
+    async checkValidationMessages(options={}){
+        this.firstNameValidationMessage= options.firstNameValidationMessage;
+        this.lastNameValidationMessage = options.lastNameValidationMessage;
+        this.emailValidationMessage= options.emailValidationMessage;
+        this.passwordValidationMessage = options.passwordValidationMessage;
+        if(options.firstNameValidationMessage) {
+            const firstNameValidationMessageBox = await Selector('label').withText('First Name').parent(0).find('p');
+            await t.expect(firstNameValidationMessageBox.innerText).eql(this.firstNameValidationMessage);
+        }
+        if(options.lastNameValidationMessage) {
+            const lastNameValidationMessageBox = await Selector('label').withText('First Name').parent(0).find('p');
+            await t.expect(lastNameValidationMessageBox.innerText).eql(this.lastNameValidationMessage);
+        }
+        if(options.emailValidationMessage) {
+            const emailValidationMessageBox = await Selector('label').withText('Email address').parent(0).find('p');
+            await t.expect(emailValidationMessageBox.innerText).eql(this.emailValidationMessage);
+        }
+        if(options.passwordValidationMessage) {
+            const passwordValidationMessageBox = await Selector('label').withText('Email address').parent(0).find('p');
+            await t.expect(passwordValidationMessageBox.innerText).eql(this.passwordValidationMessage);
+        }
     }
 }
 
--- Tests/AccountRegistration.js
+++ Tests/AccountRegistration.js
@@ -1,10 +1,16 @@
 import {Selector} from 'testcafe';
 import createAccount from '/BettyBlocks/BettyBlocks/PageModels/CreateAccount';
 
+// Import the user data from the JSON file.
+const VALIDATION_MESSAGES = require('/BettyBlocks/BettyBlocks/Fixtures/validationMessages');
+const TEST_EMAILS = require('/BettyBlocks/BettyBlocks/Fixtures/emails');
+
 fixture`Account Registration Test`
     .page`https://ui-test-app.betty.app/login`;
 
-test('Test account registration', async t => {
-
-    await createAccount.submitUserAccount({lastName:"fjgjjshsjk"});
-});
+ TEST_EMAILS.forEach(value => {
+    test('Test invalid email input', async t => {
+     await createAccount.createNewUserAccount({email: value});
+     await createAccount.checkValidationMessages({emailValidationMessage: VALIDATION_MESSAGES.invalidEmail})
+      });
+ });
