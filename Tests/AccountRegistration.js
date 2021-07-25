44caa95aeec8f933417a4b51a11f1da642e7952b
lauraamd123 <laura_andreescu10@yahoo.com>
2021-07-25 19:40:53 +0300

    added more tests for creating an account

--- Tests/AccountRegistration.js
+++ Tests/AccountRegistration.js
@@ -3,11 +3,13 @@ import createAccount from '../PageModels/CreateAccount';
 
 // Import the test data from the JSON file.
 const VALIDATION_MESSAGES = require('../TestData/validationMessages');
+const TEST_DATASET = require('../TestData/registerAccountData');
 
 const validPassword = "aA123!op";
 const validEmail = "test@test.com";
 const firstName = "testsdf";
 const lastName = "jfgsjfgjs";
+const invalidEmail="testtest.com";
 
 // Tests for the user story: "As a new customer I want to register a new account, so that I can use the application"
 fixture`Account Registration Test`
@@ -32,7 +34,7 @@ test('Test validation messages for create account with empty first name and last
 });
 
 test('Test validation messages for create account with first name, last name, invalid email and no password', async t => {
- await createAccount.createNewUserAccount({firstName: firstName, lastName: lastName, email:"testtest.com"});
+ await createAccount.createNewUserAccount({firstName: firstName, lastName: lastName, email: invalidEmail});
  await createAccount.checkValidationMessages({
      emailValidationMessage: VALIDATION_MESSAGES.invalidEmail,
      passwordValidationMessage: VALIDATION_MESSAGES.emptyField,
@@ -44,4 +46,18 @@ test('Test validation messages for create account with first name, last name, va
     await createAccount.checkValidationMessages({
         passwordValidationMessage: VALIDATION_MESSAGES.emptyField,
     })
+});
+
+TEST_DATASET.forEach(dataset => {
+    test('Test create account by inserting different invalid email values as per the test data json file and check validation messages', async t => {
+        await createAccount.createNewUserAccount({
+            firstName: dataset.firstName,
+            lastName: dataset.lastName,
+            email: dataset.email,
+            password: dataset.password
+        });
+        await createAccount.checkValidationMessages({
+            emailValidationMessage: VALIDATION_MESSAGES.invalidEmail
+        })
+    });
 });
