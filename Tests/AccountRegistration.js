85f322da4749ac824b8223d783756fc6da670768
lauraamd123 <laura_andreescu10@yahoo.com>
2021-07-25 19:18:06 +0300

    added more tests for creating an account

--- Tests/AccountRegistration.js
+++ Tests/AccountRegistration.js
@@ -1,16 +1,47 @@
 import {Selector} from 'testcafe';
-import createAccount from '/BettyBlocks/BettyBlocks/PageModels/CreateAccount';
+import createAccount from '../PageModels/CreateAccount';
 
-// Import the user data from the JSON file.
-const VALIDATION_MESSAGES = require('/BettyBlocks/BettyBlocks/Fixtures/validationMessages');
-const TEST_EMAILS = require('/BettyBlocks/BettyBlocks/Fixtures/emails');
+// Import the test data from the JSON file.
+const VALIDATION_MESSAGES = require('../TestData/validationMessages');
 
+const validPassword = "aA123!op";
+const validEmail = "test@test.com";
+const firstName = "testsdf";
+const lastName = "jfgsjfgjs";
+
+// Tests for the user story: "As a new customer I want to register a new account, so that I can use the application"
 fixture`Account Registration Test`
     .page`https://ui-test-app.betty.app/login`;
 
- TEST_EMAILS.forEach(value => {
-    test('Test invalid email input', async t => {
-     await createAccount.createNewUserAccount({email: value});
-     await createAccount.checkValidationMessages({emailValidationMessage: VALIDATION_MESSAGES.invalidEmail})
-      });
- });
+test('Test validation messages for create account with all empty fields', async t => {
+    await createAccount.createNewUserAccount();
+    await createAccount.checkValidationMessages({
+        firstNameValidationMessage: VALIDATION_MESSAGES.emptyField,
+        lastNameValidationMessage: VALIDATION_MESSAGES.emptyField,
+        passwordValidationMessage: VALIDATION_MESSAGES.emptyField,
+        emailValidationMessage: VALIDATION_MESSAGES.emptyField
+    })
+});
+
+test('Test validation messages for create account with empty first name and last name and with valid email and password', async t => {
+ await createAccount.createNewUserAccount({email: validEmail, password: validPassword});
+ await createAccount.checkValidationMessages({
+  firstNameValidationMessage: VALIDATION_MESSAGES.emptyField,
+  lastNameValidationMessage: VALIDATION_MESSAGES.emptyField,
+ })
+});
+
+test('Test validation messages for create account with first name, last name, invalid email and no password', async t => {
+ await createAccount.createNewUserAccount({firstName: firstName, lastName: lastName, email:"testtest.com"});
+ await createAccount.checkValidationMessages({
+     emailValidationMessage: VALIDATION_MESSAGES.invalidEmail,
+     passwordValidationMessage: VALIDATION_MESSAGES.emptyField,
+ })
+});
+
+test('Test validation messages for create account with first name, last name, valid email and no password', async t => {
+    await createAccount.createNewUserAccount({firstName: firstName, lastName: lastName, email:validEmail});
+    await createAccount.checkValidationMessages({
+        passwordValidationMessage: VALIDATION_MESSAGES.emptyField,
+    })
+});
